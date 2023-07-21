49e260a1ca5d809f413dcba8338fc584bfc0fe5e
Marcel Sigl <marcel-sigl@gmx.de>
2023-07-21 21:18:07 +0200

    Base is working

---
+++ Command.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using OpenAI_API;
+using OpenAI_API.Chat;
+
+namespace DesktopGBT;
+
+public class Command
+{
+    private static Dictionary<String, Action> commands;
+    private OpenAIAPI aiApi;
+    private List<ChatMessage> histroy;
+
+    internal Command(OpenAIAPI aiapi,List<ChatMessage> histroy)
+    {
+
+        commands = new Dictionary<string, Action>
+        {
+            {"help", HandleHelp},
+            {"clearConsole", HandleClearConsole},
+            {"clearHistory", HandleClearHistory},
+            {"listCommands", HandleListCommands},
+
+        };
+        this.aiApi = aiapi;
+        this.histroy = histroy;
+    }
+    public static bool HandleInput(string input)
+    {
+        if (commands.TryGetValue(input, out var command))
+        {
+            command();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void HandleHelp()
+    {
+        Console.WriteLine("There is no help, lol");
+        
+    }
+    public void HandleClearConsole()
+    {
+        Console.Clear();
+        
+        
+    }
+    public void HandleClearHistory()
+    {
+        histroy.Clear();
+        
+    }
+    public static void HandleListCommands()
+    {
+        
+        
+    }
+
+
+}
---
+++ Crypt.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DesktopGBT;
+
+internal static class Crypt
+{
+    internal static string GenerateRandomString(int length)
+    {
+        const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder res = new StringBuilder();
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] uintBuffer = new byte[4];
+
+            while (length-- > 0)
+            {
+                rng.GetBytes(uintBuffer);
+                uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                res.Append(valid[(int)(num % (uint)valid.Length)]);
+            }
+        }
+
+        return res.ToString();
+    }
+
+    internal static string Decrypt(byte[] cipherText, string encryptionKey, string iv)
+    {
+        string plaintext = null;
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(encryptionKey);
+            aes.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream(cipherText))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                using (StreamReader sr = new StreamReader(cs))
+                {
+                    plaintext = sr.ReadToEnd();
+                }
+            }
+        }
+
+        return plaintext;
+    }
+
+    internal static byte[] Encrypt(string clearText, string encryptionKey, string iv)
+    {
+        byte[] encrypted;
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(encryptionKey);
+            aes.IV = Encoding.UTF8.GetBytes(iv);
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                using (StreamWriter sw = new StreamWriter(cs))
+                {
+                    sw.Write(clearText);
+                }
+
+                encrypted = ms.ToArray();
+            }
+        }
+
+        return encrypted;
+    }
+    
+    public static bool DoesEnvironmentVariableExist(string variableName)
+    {
+        string variableValue = Environment.GetEnvironmentVariable(variableName, EnvironmentVariableTarget.User);
+        return variableValue != null;
+    }
+
+}
